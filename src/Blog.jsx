f9c1639097e95d8ed1900d5d7b466a1d71379438
Genshi0916 <genshi0916@gmail.com>
2020-05-21 21:37:12 +0900

    [追加]ライフサイクル練習

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import Article from './Article';
-
-
+import * as FooBar from './components/FooBar';
+import Hoge from './components/Hoge';
 class Blog extends React.Component{
     constructor(props){
       super(props);
       this.state={
-        isPublished:false
+        isPublished:false,
+        count:0
       }
+    }
+
+    componentDidMount(){
+      document.getElementById('counter').addEventListener('click',this.countUp)
+    }
+
+    componentDidUpdate(){
+      if(this.state.count>=10){
+        this.setState({
+          count:0
+        })
+      }
+    }
+
+    componentWillUnmount(){
+      document.getElementById('counter').removeEventListener('click',this.countUp)
+
     }
 
     togglePubliched=()=>{
@@ -16,11 +34,21 @@ class Blog extends React.Component{
       })
     };
 
+    countUp = ()=>{
+      this.setState({
+        count:this.state.count+1
+      })
+    }
+
     render(){
       return(
         <div>
           <Article
-            title={"React"} isPublished={this.state.isPublished} toggle={()=>this.togglePubliched()} />
+            title={"React"} isPublished={this.state.isPublished} toggle={()=>this.togglePubliched()}
+            count={this.state.count} />
+            <FooBar.Foo/>
+            <FooBar.Bar/>
+            <Hoge/>
         </div>
 
       )
