2077c586719b06c30f0b1ca3c772e5e006f0cd1f
Genshi0916 <genshi0916@gmail.com>
2020-05-22 00:32:40 +0900

    react hooksをいじいじしました

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -2,57 +2,42 @@ import React from 'react';
 import Article from './Article';
 import * as FooBar from './components/FooBar';
 import Hoge from './components/Hoge';
-class Blog extends React.Component{
-    constructor(props){
-      super(props);
-      this.state={
-        isPublished:false,
-        count:0
-      }
-    }
+const Blog =()=>{
 
-    componentDidMount(){
-      document.getElementById('counter').addEventListener('click',this.countUp)
-    }
+    // componentDidMount(){
+    //   document.getElementById('counter').addEventListener('click',this.countUp)
+    // }
 
-    componentDidUpdate(){
-      if(this.state.count>=10){
-        this.setState({
-          count:0
-        })
-      }
-    }
+    // componentDidUpdate(){
+    //   if(this.state.count>=10){
+    //     this.setState({
+    //       count:0
+    //     })
+    //   }
+    // }
 
-    componentWillUnmount(){
-      document.getElementById('counter').removeEventListener('click',this.countUp)
+    // componentWillUnmount(){
+    //   document.getElementById('counter').removeEventListener('click',this.countUp)
+    // }
 
-    }
+    // countUp = ()=>{
+    //   this.setState({
+    //     count:this.state.count+1
+    //   })
+    // }
 
-    togglePubliched=()=>{
-      this.setState({
-        isPublished:!this.state.isPublished
-      })
-    };
-
-    countUp = ()=>{
-      this.setState({
-        count:this.state.count+1
-      })
-    }
-
-    render(){
       return(
         <div>
           <Article
-            title={"React"} isPublished={this.state.isPublished} toggle={()=>this.togglePubliched()}
-            count={this.state.count} />
+            title={"React"}
+            // count={this.state.count}
+            />
             <FooBar.Foo/>
             <FooBar.Bar/>
             <Hoge/>
         </div>
 
       )
-    }
 }
 
 export default Blog
